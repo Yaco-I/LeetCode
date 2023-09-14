b1775ed568c1abc8754d5870b030d9b271455528
Yaco-Ibars <yacoibars97@gmail.com>
2023-09-14 19:20:19 -0300

    Update two exercise

--- LeetCode/Algorithms/Medium/2AddTwoNumbers.cs
+++ LeetCode/Algorithms/Medium/2AddTwoNumbers.cs
@@ -4,15 +4,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LeetCode.Algorithms.Medium
+namespace LeetCode.Algorithms.Medium;
+
+public static class _2AddTwoNumbers
 {
-    public static class _2AddTwoNumbers : IShowExercises
+    public static void ShowExercises()
     {
-        public static void ShowExercises()
+        //throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// It was made with github copilot only using the function name
+    /// </summary>
+    /// <param name="l1"></param>
+    /// <param name="l2"></param>
+    /// <returns></returns>
+    public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        ListNode result = new ListNode();
+        ListNode current = result;
+        int carry = 0;
+        while (l1 != null || l2 != null)
         {
-            //throw new NotImplementedException();
+            int sum = carry;
+            if (l1 != null)
+            {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null)
+            {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+            carry = sum / 10;
         }
+        if (carry > 0)
+            current.next = new ListNode(carry);
+        return result.next;
+    }
 
 
+
+}
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
     }
 }
