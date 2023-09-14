8281b8a3993472711499d88190e362705fb890e7
Yaco-Ibars <yacoibars97@gmail.com>
2023-09-14 18:40:45 -0300

    Add project files.

---
+++ LeetCode/Algorithms/Easy/1TwoSum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms.Easy;
+
+public class _1TwoSum
+{
+
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (i == j)
+                    continue;
+                if (nums[i] + nums[j] == target)
+                    return new int[] { i, j };
+            }
+        }
+        return null;
+    }
+
+}
---
+++ LeetCode/Program.cs
@@ -0,0 +1,79 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LeetCode.Algorithms.Easy;
+
+Console.WriteLine("Hello, World!");
+
+//Exercices.E1282View(new int[] { 3, 3, 3, 3, 3, 1, 3 });
+Exercices.ViewArray(_1TwoSum.TwoSum(new int[] { 3, 3 }, 6));
+Exercices.ViewArray(_1TwoSum.TwoSum(new int[] { 2, 7, 11, 15 }, 9));
+
+
+
+public class Exercices
+{
+    public static int E1572(int[][] array)
+    {
+        var result = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (i != array.Length - i - 1)
+                result += array[i][i] + array[i][array.Length - 1 - i];
+            else
+                result += array[i][i];
+        }
+        return result;
+    }
+
+    // 1 + 1     1+3
+    // 2 + 2     
+    // 3 + 3 
+    //
+    public static  IList<IList<int>> E1282(int[] groupSizes)
+    {
+        List<IList<int>> result = new List<IList<int>>();
+        Dictionary<int, List<int>> groupMap = new Dictionary<int, List<int>>();
+
+        for (int i = 0; i < groupSizes.Length; i++)
+        {
+            int size = groupSizes[i];
+
+            if (!groupMap.ContainsKey(size))
+            {
+                groupMap[size] = new List<int>();
+            }
+
+            groupMap[size].Add(i);
+
+            if (groupMap[size].Count == size)
+            {
+                result.Add(groupMap[size]);
+                groupMap[size] = new List<int>();
+            }
+        }
+
+        return result;
+    }
+
+    public static void ViewArray(int[] array)
+    {
+        foreach (int number in array)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+    }
+
+
+    public static void E1282View(int[] array) {
+        var result = E1282(array);
+        foreach (List<int> innerList in result)
+        {
+            foreach (int number in innerList)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine(); // Agrega una nueva línea después de cada lista interna
+        }
+    }
+
+}
