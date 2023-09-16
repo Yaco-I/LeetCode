dd65448d567653f65f5cd6cea3ffb1b579b53692
Yaco-Ibars <yacoibars97@gmail.com>
2023-09-16 10:59:20 -0300

    PalindromeNumber

--- LeetCode/Algorithms/Easy/9PalindromeNumber.cs
+++ LeetCode/Algorithms/Easy/9PalindromeNumber.cs
@@ -8,10 +8,21 @@ namespace LeetCode.Algorithms.Easy
 {
     public static class _9PalindromeNumber
     {
-        public static void PalindromeNumber(int numero)
+        public static bool PalindromeNumber(int numero)
         {
-            
-            
+            string num = numero.ToString();
+            int valor = num.Length / 2;
+
+            for (int i = 0; i < valor ; i++)
+            {
+                if (num[i] != num[num.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+            return true;
+
+
         }
     }
 }
