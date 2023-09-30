2b7c4e90406eb6e4a13749a5a379c2e1e813282f
Yaco-Ibars <yacoibars97@gmail.com>
2023-09-30 13:17:17 -0300

    58 Length of last word

---
+++ LeetCode.Test/0-100/UnitTest58_LengthOfLastWord.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Algorithms._0_100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Test._0_100
+{
+    [TestClass]
+    public class UnitTest58_LengthOfLastWord
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var input = "Hello World";
+            var expected = 5;
+            var actual = LengthOfLastWord.LengthOfLastWordMethod(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
