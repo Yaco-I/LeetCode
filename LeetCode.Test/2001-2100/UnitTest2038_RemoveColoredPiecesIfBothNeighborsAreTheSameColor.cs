97cd3600382762c671e07f81f4b03773fa7e24e7
Yaco-Ibars <yacoibars97@gmail.com>
2023-10-02 11:57:33 -0300

    RemoveColoredPiecesIfBothNeighborsAreTheSameColor

---
+++ LeetCode.Test/2001-2100/UnitTest2038_RemoveColoredPiecesIfBothNeighborsAreTheSameColor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Test._2001_2100
+{
+    [TestClass]
+    public class UnitTest2038_RemoveColoredPiecesIfBothNeighborsAreTheSameColor
+    {
+        [TestMethod]
+        public void UnitTest2038_WinnerOfGame()
+        {
+            string colors = "AAABABB";
+            bool expected = true;
+            bool actual = LeetCode.Algorithms._2001_2100.RemoveColoredPiecesIfBothNeighborsAreTheSameColor.WinnerOfGame(colors);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ LeetCode/Algorithms/2001-2100/2038_RemoveColoredPiecesIfBothNeighborsAreTheSameColor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms._2001_2100
+{
+    public class RemoveColoredPiecesIfBothNeighborsAreTheSameColor
+    {
+        public static bool WinnerOfGame(string colors)
+        {
+            int alice = 0;
+            int bob = 0;
+            for (int i = 1; i < colors.Length - 1; i++)
+            {
+                if (colors[i - 1] == colors[i] && colors[i] == colors[i + 1])
+                {
+                    if (colors[i] == 'A')
+                    {
+                        alice++;
+                    }
+                    else
+                    {
+                        bob++;
+                    }
+                }
+            }
+            return alice > bob;
+        }
+    }
+}
