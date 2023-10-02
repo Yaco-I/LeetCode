360dc82d5c2cffe11ddaf6796346e1fd5557809c
Yaco-Ibars <yacoibars97@gmail.com>
2023-10-02 11:58:30 -0300

    Update

--- LeetCode/Algorithms/2001-2100/2038_RemoveColoredPiecesIfBothNeighborsAreTheSameColor.cs
+++ LeetCode/Algorithms/2001-2100/2038_RemoveColoredPiecesIfBothNeighborsAreTheSameColor.cs
@@ -10,23 +10,23 @@ namespace LeetCode.Algorithms._2001_2100
     {
         public static bool WinnerOfGame(string colors)
         {
-            int alice = 0;
-            int bob = 0;
+            int count = 0;
+            
             for (int i = 1; i < colors.Length - 1; i++)
             {
                 if (colors[i - 1] == colors[i] && colors[i] == colors[i + 1])
                 {
                     if (colors[i] == 'A')
                     {
-                        alice++;
+                        count++;
                     }
                     else
                     {
-                        bob++;
+                        count--;
                     }
                 }
             }
-            return alice > bob;
+            return count > 0;
         }
     }
 }
