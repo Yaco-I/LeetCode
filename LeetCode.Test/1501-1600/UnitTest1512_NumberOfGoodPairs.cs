f1ec68dd0ee1d1ac78802f199565ed3d33a91419
Yaco-Ibars <yacoibars97@gmail.com>
2023-10-03 12:38:37 -0300

    RemoveColoredPiecesIfBothNeighborsAreTheSameColor

---
+++ LeetCode.Test/1501-1600/UnitTest1512_NumberOfGoodPairs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.Algorithms._1501_1600;
+
+namespace LeetCode.Test._1501_1600
+{
+    [TestClass]
+    public class UnitTest1512_NumberOfGoodPairs
+    {
+        [TestMethod]
+        public void TestMethod_NumIdenticalPairsTest()
+        {
+            var input = new int[] { 1, 2, 3, 1, 1, 3 };
+            var output = NumberOfGoodPairs.NumIdenticalPairs(input);
+            Assert.AreEqual(4, output);
+        }
+    }
+}
