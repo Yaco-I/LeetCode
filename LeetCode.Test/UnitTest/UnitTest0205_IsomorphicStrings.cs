06072c266e218edb21e2f58db04b06ef2d8d40d9
Yaco-Ibars <yacoibars97@gmail.com>
2023-10-06 11:24:29 -0300

    0205 IsomorphicStrings

---
+++ LeetCode.Test/UnitTest/UnitTest0205_IsomorphicStrings.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Test.UnitTest
+{
+    [TestClass]
+    public class UnitTest0205_IsomorphicStrings
+    {
+        [TestMethod]
+        public void TestMethod_IsIsomorphic()
+        {
+            var result1 = IsomorphicStrings.IsIsomorphic("egg", "add");
+            var result2 = IsomorphicStrings.IsIsomorphic("foo", "bar");
+            var result3 = IsomorphicStrings.IsIsomorphic("paper", "title");
+
+            Assert.IsTrue(result1);
+            Assert.IsFalse(result2);
+            Assert.IsTrue(result3);
+        }
+
+    }
+}
---
+++ LeetCode/Algorithms/0205_IsomorphicStrings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms
+{
+    public class IsomorphicStrings
+    {
+        public static bool IsIsomorphic(string s, string t)
+        {
+            Dictionary<char, char> mapS = new Dictionary<char, char>();
+            Dictionary<char, char> mapT = new Dictionary<char, char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!mapS.ContainsKey(s[i]))
+                {
+                    if (mapT.ContainsKey(t[i]))
+                    {
+                        return false;
+                    }
+                    mapS.Add(s[i], t[i]);
+                    mapT.Add(t[i], s[i]);
+                }
+                else
+                {
+                    if (mapS[s[i]] != t[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
