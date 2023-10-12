661499d0f585de089b865427e11a6c87d4d6ef94
Yaco-Ibars <yacoibars97@gmail.com>
2023-10-12 09:59:00 -0300

    AccountBalanceAfterRoundedPurchas

---
+++ LeetCode.Test/UnitTest/UnitTest2806_AccountBalanceAfterRoundedPurchas.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Test.UnitTest
+{
+    [TestClass]
+    public class UnitTest2806_AccountBalanceAfterRoundedPurchas
+    {
+        [TestMethod]
+        public void TestMethodAccountBalanceAfterPurchase()
+        {
+            int purchaseAmount = 4;
+            int result = AccountBalanceAfterRoundedPurchas.AccountBalanceAfterPurchase(purchaseAmount);
+            Assert.AreEqual(100, result);
+
+             purchaseAmount = 11;
+            result = AccountBalanceAfterRoundedPurchas.AccountBalanceAfterPurchase(purchaseAmount);
+            Assert.AreEqual(90, result);
+
+
+            purchaseAmount = 15;
+            result = AccountBalanceAfterRoundedPurchas.AccountBalanceAfterPurchase(purchaseAmount);
+            Assert.AreEqual(80, result);
+            purchaseAmount = 5;
+            result = AccountBalanceAfterRoundedPurchas.AccountBalanceAfterPurchase(purchaseAmount);
+            Assert.AreEqual(90, result);
+        }
+    }
+}
---
+++ LeetCode/Algorithms/2806_AccountBalanceAfterRoundedPurchas.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms
+{
+    
+    public class AccountBalanceAfterRoundedPurchas
+    {
+        public static int AccountBalanceAfterPurchase(int purchaseAmount)
+        {
+            //if (purchaseAmount == 0)
+            //    return 100;
+            string purchaseAmountString = purchaseAmount.ToString();
+            int lastDigit = int.Parse(purchaseAmountString[purchaseAmountString.Length - 1].ToString());
+            if (lastDigit >= 5)
+            {
+                purchaseAmount += 10 - lastDigit;
+            }
+            else{
+                purchaseAmount -= lastDigit;
+            }
+
+            return 100 - purchaseAmount;
+
+            //decimal purchaseAmountDecimal = (decimal)(purchaseAmount) / 10;
+            //purchaseAmountDecimal = Math.Round(purchaseAmountDecimal);
+            //return 100 - int.Parse(purchaseAmountDecimal.ToString()) * 10 ;
+        }
+    }
+}
