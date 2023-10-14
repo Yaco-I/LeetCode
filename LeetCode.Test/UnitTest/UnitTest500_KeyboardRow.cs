19742172c8dec6fe34d09debb63cb680d74414e5
Yaco-Ibars <yacoibars97@gmail.com>
2023-10-14 19:47:15 -0300

    Keyboard row

---
+++ LeetCode.Test/UnitTest/UnitTest500_KeyboardRow.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Test.UnitTest
+{
+    [TestClass]
+    public class UnitTest500_KeyboardRow
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            string[] words = new string[] { "Hello", "Alaska", "Dad", "Peace" };
+            string[] expected = new string[] { "Alaska", "Dad" };
+
+            // Act
+            
+            string[] actual = KeyboardRow.FindWords(words);
+
+            // Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ LeetCode/Algorithms/0500_KeyboardRow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms
+{
+    public class KeyboardRow
+    {
+        public static string[] FindWords(string[] words)
+        {
+            List<string> result = new List<string>();
+
+            foreach(string word in words)
+            {
+                string lowerWord = word.ToLower();
+
+                if (lowerWord.All(c => "qwertyuiop".Contains(c)) ||
+                                       lowerWord.All(c => "asdfghjkl".Contains(c)) ||
+                                                          lowerWord.All(c => "zxcvbnm".Contains(c)))
+                {
+                    result.Add(word);
+                }
+            }
+            return result.ToArray();
+        }
+    }
+}
