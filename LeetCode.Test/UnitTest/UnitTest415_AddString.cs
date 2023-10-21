4d946587c43364c0238100eaf8438f4dacc6bd5e
Yaco-Ibars <yacoibars97@gmail.com>
2023-10-21 13:46:24 -0300

    AddString

---
+++ LeetCode.Test/UnitTest/UnitTest415_AddString.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.Algorithms;
+
+namespace LeetCode.Test.UnitTest;
+
+[TestClass]
+public class UnitTest415_AddString
+{
+    [TestMethod]
+    public void TestMethod()
+    {
+        var result = AddString.AddStrings("11", "123");
+        Assert.AreEqual(result, "134");
+
+
+        result = AddString.AddStrings("456", "77");
+        Assert.AreEqual(result, "533");
+
+        result = AddString.AddStrings("0", "0");
+        Assert.AreEqual(result, "0");
+
+        result = AddString.AddStrings("1", "9");
+        Assert.AreEqual("10", result);
+
+        result = AddString.AddStrings("125", "99");
+        Assert.AreEqual(result, "224");
+
+
+
+    }
+}
---
+++ LeetCode/Algorithms/0415_AddString.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace LeetCode.Algorithms;
+
+public class AddString
+{
+
+    public static string AddStrings(string num1, string num2)
+    {
+        int Length1 = num1.Length;
+        int Length2 = num2.Length;
+        int Length = Math.Max(Length1, Length2);
+        int Carry = 0;
+        int digit = 0;
+        StringBuilder Result = new StringBuilder();
+        for (int i = 1; i <= Length; i++)
+        {
+            digit = 0;
+            if (i <= Length1)
+            {
+                digit += int.Parse(num1[Length1 - i].ToString());
+            }
+            if (i <= Length2)
+            {
+                digit += int.Parse(num2[Length2 - i].ToString());
+
+            }
+            if (Carry > 0)
+            {
+                digit += Carry;
+                Carry = 0;
+            }
+            if (digit > 9)
+            {
+                Carry = 1;
+                digit -= 10;
+            }
+            Result.Insert(0, digit);
+        }
+        if(Carry > 0)
+        {
+            Result.Insert(0, Carry);
+        }
+        return Result.ToString();
+    }
+}
