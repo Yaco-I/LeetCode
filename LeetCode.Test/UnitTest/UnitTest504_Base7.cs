03f69ebda8d46a838b38feb8674ff5368bd65132
Yaco-Ibars <yacoibars97@gmail.com>
2023-10-22 08:45:16 -0300

    ConvertToBase7

---
+++ LeetCode.Test/UnitTest/UnitTest504_Base7.cs
@@ -0,0 +1,28 @@
+﻿
+using LeetCode.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Test.UnitTest
+{
+    [TestClass]
+    public class UnitTest504_Base7
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            int num = 100;
+            string expected = "202";
+
+            // Act
+            string actual = ConvertToBase7.ConvertToBase7Method(num);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ LeetCode/Algorithms/0504_ConvertToBase7.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms
+{
+
+    public class ConvertToBase7
+    {
+        public static string ConvertToBase7Method(int num)
+        {
+            var result = num;
+            int sum = 0;
+            int count = 0;
+            while(result > 6 || result < -6)
+            {
+                sum += (result % 7) * (int)Math.Pow(10, count);
+                count++;
+                result = result / 7;
+            }
+            sum += result * (int)Math.Pow(10, count + 1);
+            return sum.ToString();
+        }
+    }
+}
