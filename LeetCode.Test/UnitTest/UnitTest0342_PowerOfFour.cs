5de80b96856c189092fc9bd51d66da697f1ab304
Yaco-Ibars <yacoibars97@gmail.com>
2023-10-23 12:38:55 -0300

    Power of Four

---
+++ LeetCode.Test/UnitTest/UnitTest0342_PowerOfFour.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Test.UnitTest
+{
+    [TestClass]
+    public class UnitTest0342_PowerOfFour
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            int n = 16;
+            bool expected = true;
+
+            // Act
+            bool actual = PowerOfFour.IsPowerOfFour(n);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ LeetCode/Algorithms/0342_PowerOfFour.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms
+{
+    public class PowerOfFour
+    {
+        public static bool IsPowerOfFour(int n)
+        {
+            bool result = false;
+            decimal nDecimal = n;
+
+            if (nDecimal == 1)
+            {
+                result = true;
+            }
+
+            while (nDecimal > 1)
+            {
+                nDecimal = nDecimal / 4;
+                if (nDecimal == 1)
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+    }
+}
