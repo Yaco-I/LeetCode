a266211e098f09881cff07c24a4bd0f2585df1d1
Yaco-Ibars <yacoibars97@gmail.com>
2023-10-26 19:02:04 -0300

    Reverse string

---
+++ LeetCode.Test/UnitTest/UnitTest0344_ReverseString.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Test.UnitTest
+{
+    [TestClass]
+    public class UnitTest0344_ReverseString
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            char[] arr = { 'h', 'e', 'l', 'l', 'o' };
+            char[] expected = { 'o', 'l', 'l', 'e', 'h' };
+
+            ReverseString.ReverseStringMethod(arr);
+
+            Assert.AreEqual(arr.Length, expected.Length);
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], arr[i]);
+
+            }
+        }
+    }
+}
---
+++ LeetCode/Algorithms/0344_ReverseString.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms
+{
+    public class ReverseString
+    {
+        public static void ReverseStringMethod(char[] s)
+        {
+            int left = 0;
+            int right = s.Length - 1;
+
+            while (left < right)
+            {
+                // Intercambiar los caracteres en las posiciones left y right
+                char temp = s[left];
+                s[left] = s[right];
+                s[right] = temp;
+
+                // Mover los punteros hacia el centro de la cadena
+                left++;
+                right--;
+            }
+
+
+        }
+    }
+}
