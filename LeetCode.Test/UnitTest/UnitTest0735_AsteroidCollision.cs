a4e15af4e3b7c876de614ffa3d9ddb73b3449ec5
Yaco-Ibars <yacoibars97@gmail.com>
2023-10-27 13:12:53 -0300

    Update AsteroidCollision

---
+++ LeetCode.Test/UnitTest/UnitTest0735_AsteroidCollision.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Test.UnitTest
+{
+    [TestClass]
+    public class UnitTest0735_AsteroidCollision
+    {
+        [TestMethod]
+        public void TestMethod_AsteroidCollision()
+        {
+            
+            int[] asteroids = { 5, 10, -5 };
+            int[] expected = { 5, 10 };
+            int[] result = AsteroidCollision.AsteroidCollisionMethod(asteroids);
+            Assert.AreEqual(expected.Length, result.Length);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], result[i]);
+            }
+        }
+    }
+}
---
+++ LeetCode/Algorithms/0735_AsteroidCollision.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms
+{
+    public class AsteroidCollision
+    {
+        public static int[] AsteroidCollisionMethod(int[] asteroids)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            foreach (int asteroid in asteroids)
+            {
+                if (asteroid > 0)
+                {
+                    stack.Push(asteroid);
+                }
+                else
+                {
+                    while (stack.Count > 0 && stack.Peek() > 0 && stack.Peek() < Math.Abs(asteroid))
+                    {
+                        stack.Pop();
+                    }
+
+                    if (stack.Count > 0 && stack.Peek() == Math.Abs(asteroid))
+                    {
+                        stack.Pop();
+                    }
+                    else if (stack.Count == 0 || stack.Peek() < 0)
+                    {
+                        stack.Push(asteroid);
+                    }
+                }
+            }
+
+            return stack.Reverse().ToArray();
+        }
+    }
+}
