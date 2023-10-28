f2e66ffc679a1cfc2f0149d1ed217c7ad2dde2ff
Yaco-Ibars <yacoibars97@gmail.com>
2023-10-28 19:40:11 -0300

    75 SortColors

---
+++ LeetCode.Test/UnitTest/UnitTest0075_SortColors.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Test.UnitTest
+{
+    [TestClass]
+    public class UnitTest0075_SortColors
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            var nums = new int[] { 2, 0, 2, 1, 1, 0 };
+            SortColors.SortColorsMethod(nums);
+            var expected = new int[] { 0, 0, 1, 1, 2, 2 };
+            Assert.AreEqual(expected.Length, nums.Length);
+        }
+    }
+}
---
+++ LeetCode/Algorithms/0075_SortColors.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms
+{
+    public class SortColors
+    {
+        public static void SortColorsMethod(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length - i - 1; j++)
+                {
+                    if (nums[j] > nums[j + 1])
+                    {
+                        int temp = nums[j];
+                        nums[j] = nums[j + 1];
+                        nums[j + 1] = temp;
+                    }
+                }
+            }
+
+        }
+
+    }
+}
